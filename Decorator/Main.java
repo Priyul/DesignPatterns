7bee1f14e96c42ef2be6240e75ba609541778ba7
Priyul <Priyul20@gmail.com>
2022-11-11 20:09:59 +0200

    🔥 testing decorator

--- Decorator/Main.java
+++ Decorator/Main.java
@@ -12,6 +12,10 @@ public class Main {
 
         CaramelDecorator addCaramel = new CaramelDecorator(decaf);
         System.out.println("Decaf price after being decorated with caramel: R" + addCaramel.cost());
+        CaramelDecorator add2Caramel = new CaramelDecorator(addCaramel);
+        System.out.println("Decaf price after being decorated with TWO caramel: R" + add2Caramel.cost());
+        CaramelDecorator add3Caramel = new CaramelDecorator(add2Caramel);
+        System.out.println("Decaf price after being decorated with THREE caramel: R" + add3Caramel.cost());
 
     }
 }
