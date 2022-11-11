b228150e4e8063a5b4806d91bca8d3a8b9c00392
Priyul <Priyul20@gmail.com>
2022-11-11 11:14:41 +0200

    🚑 added flyweight

--- Flyweight/Hunter.java
+++ Flyweight/Hunter.java
@@ -2,19 +2,20 @@ package Flyweight;
 
 public class Hunter {
     public static void main(String[] args) {
-        System.out.println("Shooting with cached bullets");
-
-        Gun shotGun = new ShotGun(8, BulletType.SHOTGUN_BULLET);
-        shotGun.loadBullets();
-        shotGun.shoot();
-        /* each bullet shot is the same  */
-
         System.out.println("Shooting with uncached bullets");
 
         Gun shotGunUncached = new ShotGun(8, BulletType.SHOTGUN_BULLET);
         shotGunUncached.loadUncachedBullets();
         shotGunUncached.shoot();
         /* each bullet shot is different */
+
+        System.out.println("Shooting with cached bullets");
+
+        Gun shotGun = new ShotGun(8, BulletType.SHOTGUN_BULLET);
+        shotGun.loadBullets();
+        shotGun.shoot();
+        /* each bullet shot is the same  */
+        
         /* therefore uncached shotgun consumes 8x more memory than cached shotgun bullets */
     
     }
